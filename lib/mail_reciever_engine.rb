7407a5e2facd17f6515bcbb95b6e264b81c6a78a
David Guthu <davidguthu@gmail.com>
2012-03-07 13:14:57 -0500

    Fixed deprecation warnings

--- lib/mail_reciever_engine.rb
+++ lib/mail_reciever_engine.rb
@@ -7,11 +7,11 @@ class MailRecieverEngine < Rails::Engine
   ::ActionMailer::Base.add_delivery_method :mail_reciever_engine, MailRecieverEngine::Reciever
   
   initializer "paths" do
-    paths.mail_reciever_datastore Rails.root + "db/mail_reciever_engine.#{Rails.env}.yml"
+    paths['mail_reciever_datastore'] = Rails.root + "db/mail_reciever_engine.#{Rails.env}.yml"
 
     config.mail_reciever_engine_moneta_backend = :YAML unless config.respond_to? :mail_reciever_engine_moneta_backend
     config.mail_reciever_engine_moneta_config = {
-      :path => config.paths.mail_reciever_datastore.first
+      :path => config.paths['mail_reciever_datastore'].first
     } unless config.respond_to? :mail_reciever_engine_moneta_config
   end
 end
